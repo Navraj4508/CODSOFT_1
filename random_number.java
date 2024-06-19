bf3533e66f4524dd13230f3fa034cb62ccfa8fc6
Navraj4508 <166549018+Navraj4508@users.noreply.github.com>
2024-06-19 18:35:13 +0530

    Add files via upload

---
+++ random_number.java
@@ -0,0 +1,47 @@
+package Number_game;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class random_number {
+
+	public static void main(String[] args) {
+		Random rd = new Random();    //create new instance of the 'Random'. 
+		int number_guess = rd.nextInt(100)+1;     //generates a number between 0 and 99, adding 1
+		System.out.println("Welcome to the guess number game!");
+		System.out.println("Guess a number between 1 to 100!");
+		Scanner sc = new Scanner(System.in);
+		int guess;
+		int maxattempts = 5;
+		int attempts = 0;
+		
+		
+
+		while(attempts<maxattempts) {
+			guess = sc.nextInt(); // input from user who play guess number game.
+			attempts++;
+			if(guess == number_guess) {
+				System.out.println("Conguration! guess is correct number!");
+				break;
+			}
+			if(guess < number_guess) {
+				System.out.println("Oops! guess is too low!");
+				System.out.println("Try Again!");
+			}
+			else {
+				System.out.println("Oops! guess is too high!");
+				System.out.println("Try Again!");
+			}
+		}
+		
+		
+		if (attempts == maxattempts) {
+            System.out.println("Sorry, you've reached the maximum number of attempts. The correct number was " + number_guess + ".");
+        }
+		
+		
+		
+
+	}
+
+}
