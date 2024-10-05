13e9814ba5cb49e480116d112aa7ae1b5e6676c4
Navraj4508 <166549018+Navraj4508@users.noreply.github.com>
2024-10-05 18:52:07 +0530

    Update random_number.java

--- random_number.java
+++ random_number.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class random_number {
 
 	public static void main(String[] args) {
-		Random rd = new Random();    //create new instance of the 'Random'. 
+		/*Random rd = new Random();    //create new instance of the 'Random'. 
 		int number_guess = rd.nextInt(100)+1;     //generates a number between 0 and 99, adding 1
 		System.out.println("Welcome to the guess number game!");
 		System.out.println("Guess a number between 1 to 100!");
@@ -17,7 +17,7 @@ public class random_number {
 		
 		
 
-		while(attempts<maxattempts) {
+		/*while(attempts<maxattempts) {
 			guess = sc.nextInt(); // input from user who play guess number game.
 			attempts++;
 			if(guess == number_guess) {
@@ -33,12 +33,14 @@ public class random_number {
 				System.out.println("Try Again!");
 			}
 		}
-		
+		*/
 		
 		if (attempts == maxattempts) {
             System.out.println("Sorry, you've reached the maximum number of attempts. The correct number was " + number_guess + ".");
         }
-		
+	*/
+
+		System.out.println("Hello");
 		
 		
 
